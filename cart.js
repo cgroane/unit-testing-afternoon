0c6a5bf0b8ce3f40fa79005cbd2fafd4c00010dc
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2017-12-14 14:43:33 -0600

    test passed

--- cart.js
+++ cart.js
@@ -1,18 +1,21 @@
 const cars = require('./data/cars');
 
 module.exports = {
-  cart: null,
-  total: null,
+  cart: [],
+  total: 0,
 
-  addToCart: function() {
-
+  addToCart: function(obj) {
+    this.cart.push(obj)
+    this.total += obj.price
   },
 
-  removeFromCart: function() {
-
+  removeFromCart: function(ind, price) {
+    this.cart.splice(ind, 1)
+    this.total -= price
   },
   
   checkout: function() {
-
+    this.cart = [];
+    this.total = 0
   }
 };
